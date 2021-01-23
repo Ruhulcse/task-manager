3efdb707e861fcbfc898a4a768776225dbfd8556
Ruhulcse <ruhul.cse7862@gmail.com>
2021-01-23 15:56:15 +0600

    Task manager completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import TaskListContextProvider from "../contexts/TaskListContext";
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+
 import "../App.css";
-function App() {
+import Header from "./Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>app testing</h1>
-    </div>
+    <TaskListContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <Header />
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </TaskListContextProvider>
   );
-}
+};
 
 export default App;
--- src/context/TaskListContext.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState } from "react";
-
-export const TaskListContext = createContext();
-
-const TaskListContextProvider = () => {
-  return <div>Task list context</div>;
-};
-
-export default TaskListContextProvider;
